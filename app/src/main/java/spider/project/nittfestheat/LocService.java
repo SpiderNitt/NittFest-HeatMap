cbbbe94a2ac481a02c7ec29151340a05445b0324
praba1110 <praba1110@gmail.com>
2016-03-28 19:49:52 +0530

    AsyncTask done

--- app/src/main/java/spider/project/nittfestheat/LocService.java
+++ app/src/main/java/spider/project/nittfestheat/LocService.java
@@ -18,6 +18,14 @@ import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Calendar;
+
 /**
  * Created by praba1110 on 28/3/16.
  */
@@ -29,6 +37,7 @@ public class LocService extends Service implements LocationListener {
     boolean gps_enabled, network_enabled, thread_stop = false;
     String latitude, longitude;
     Thread thread;
+    int lastsavedseconds;
 
     @Nullable
     @Override
@@ -62,23 +71,32 @@ public class LocService extends Service implements LocationListener {
         location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         final Location finalLocation = location;
-
+        final Calendar c=Calendar.getInstance();
+        lastsavedseconds=c.get(Calendar.SECOND);
         thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 while (!thread_stop) {
-                    try {
-                        //every 30mins sends data
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
 
-                    onLocationChanged(finalLocation);
+                    //every 30mins sends data
+                    if(lastsavedseconds-c.get(Calendar.SECOND)>=10){
+                        lastsavedseconds=c.get(Calendar.SECOND);
+
+                        onLocationChanged(finalLocation);
+
+                    }
+                    // Log.d("Thread","Thread");
+
+
+
+                    //new AsyncSendData().execute(latitude, longitude);
+                    //
                 }
             }
         });
         thread.start();
+
+
     }
 
     @Override
@@ -86,11 +104,12 @@ public class LocService extends Service implements LocationListener {
 
         latitude = String.valueOf(location.getLatitude());
         longitude = String.valueOf(location.getLongitude());
-        //new AsyncSendData().execute(latitude, longitude);
 
+        Log.d("Loc", "Lat " + latitude + " Long " + longitude);
         //Toast.makeText(this, "Lat "+latitude+" Long "+longitude,
         //        Toast.LENGTH_SHORT).show();
-        Log.d("Loc", "Lat " + latitude + " Long " + longitude);
+        new AsyncSendData().execute(latitude,longitude,"106114100");
+
     }
 
     @Override
@@ -122,7 +141,39 @@ public class LocService extends Service implements LocationListener {
 
 
         @Override
-        protected Void doInBackground(String... params) {
+        protected Void doInBackground(String... params)
+        {
+            String result = "";
+            try {
+                String link = "http://b1a7b36a.ngrok.io/add";
+                String latitude = params[0];
+                String longitude = params[1].toLowerCase();
+                String rollno = params[2];
+
+                URL URL=new URL(link);
+                HttpURLConnection urlConnection= (HttpURLConnection) URL.openConnection();
+                String postParameters="lat="+latitude+"&long="+longitude
+                        +"&rollno="+rollno;
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type",
+                        "application/x-www-form-urlencoded");
+                urlConnection.setFixedLengthStreamingMode(postParameters.getBytes().length);
+                PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
+                out.print(postParameters);
+                out.close();
+                InputStream inputStream=urlConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+                String line="";
+                while((line=bufferedReader.readLine())!=null){
+                    Log.d("line",line);
+                    result+=line;
+                }
+                Log.d("DATA",result);
+                inputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
             return null;
         }
     }
--- app/src/main/java/spider/project/nittfestheat/MainActivity.java
+++ app/src/main/java/spider/project/nittfestheat/MainActivity.java
@@ -7,11 +7,13 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startService(new Intent(this,LocService.class));
+        Intent intent= new Intent(this,LocService.class);
+        startService(intent);
     }
     public void stopService(View V){
         stopService(new Intent(getBaseContext(),LocService.class));
